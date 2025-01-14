48df0720de9349d14da134dcdbe6623c4c58b744
Abu Taher Siddiki Adnan <adnan02802@gamil.com>
2025-01-14 11:02:48 +0600

    Updated

--- calc_project/settings.py
+++ calc_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%4=3nwn_36)@m+2fn@%4+4%#4$xxwrkuomu!7mi$!g5awo_nwu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
